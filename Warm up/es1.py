8cccd0fb77e3dc69ce6fbfc8ee23955d270728ca
Sif247 <sif-isma2002@hotmail.com>
2024-09-27 16:20:38 +0200

    First Exercise

--- Warm up/es1.py
+++ Warm up/es1.py
@@ -1,11 +1,12 @@
 #Scrivi uno script che accetta un input consistente in un numero intero rappresentante
 #un raggio, calcola l’area del rispettivo cerchio e stampa il risultato.
 
+PI = 3.14
+def area():
+    r = float(input("digit the value of radius: "))
 
-r = 0
-input(r)
-a = r * r * 3.14
-print("il risultato è: ")
-print(a)
+    ris = r * r * PI
+    print("l'area è: " + str(ris))
+    return ris
 
-
+area()
