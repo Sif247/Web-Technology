3352b6067f45f513c0e2a7ef9b63ce218128e033
Sif247 <sif-isma2002@hotmail.com>
2024-09-27 13:29:34 +0200

    Exercises of first week

---
+++ Warm up/es1.py
@@ -0,0 +1,11 @@
+#Scrivi uno script che accetta un input consistente in un numero intero rappresentante
+#un raggio, calcola l’area del rispettivo cerchio e stampa il risultato.
+
+
+r = 0
+input(r)
+a = r * r * 3.14
+print("il risultato è: ")
+print(a)
+
+
